c2088cefc0ec58f96728e6aed55abd9c7b28eee8
redouane <redouane@DESKTOP-4NL56A8>
2018-01-26 18:11:27 +0000

    add a comment

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v;
+            Console.WriteLine("jdhfk");
+        }
+    }
+}
